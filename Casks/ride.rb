33a5a517914fe05a0f5b9544727654728916d6f7
Stefan Kruger <stefan@dyalog.com>
2023-05-15 15:02:44 +0100

    try version fix

--- Casks/ride.rb
+++ Casks/ride.rb
@@ -1,8 +1,8 @@
 cask "ride" do
-  version "4.4"
+  version "4.4.3732"
   sha256 "e3aba4ab80396f6902ddde67369d221bd855071596edda3ab410374bdda7ced6"
 
-  url "https://github.com/Dyalog/ride/releases/download/v4.4.3732/ride-#{version}.3732_mac.pkg"
+  url "https://github.com/Dyalog/ride/releases/download/v4.4.3732/ride-#{version}_mac.pkg"
   name "ride"
   desc "RIDE: the Remote IDE for Dyalog APL"
   homepage "https://github.com/Dyalog/ride"
