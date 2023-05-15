d6064223548d94bcdca18d45d392ca3566c35a70
Stefan Kruger <stefan@dyalog.com>
2023-05-15 16:03:52 +0100

    try again, but with a .dmg

---
+++ Casks/ride44.rb
@@ -0,0 +1,11 @@
+cask "ride44" do
+  version "4.4"
+  sha256 "d90c8ec39ae246b9a47eefa4734314ff685b8694196af67911e6431b56d6aa8f"
+
+  url "https://github.com/xpqz/releases/releases/download/ride#{version}/ride#{version}.dmg"
+  name "ride4.4"
+  desc ""
+  homepage ""
+
+  app "Ride-4.4.app"
+end
