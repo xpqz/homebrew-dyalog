f05ef526407626d0744653864826d3eec5c787bb
Stefan Kruger <stefan@dyalog.com>
2023-05-15 14:55:53 +0100

    Only use major.minor and specify app name

--- Casks/ride.rb
+++ Casks/ride.rb
@@ -1,11 +1,11 @@
 cask "ride" do
-  version "4.4.3732_mac"
+  version "4.4"
   sha256 "e3aba4ab80396f6902ddde67369d221bd855071596edda3ab410374bdda7ced6"
 
-  url "https://github.com/Dyalog/ride/releases/download/v4.4.3732/ride-#{version}.pkg"
+  url "https://github.com/Dyalog/ride/releases/download/v4.4.3732/ride-#{version}.3732_mac.pkg"
   name "ride"
-  desc ""
-  homepage ""
+  desc "RIDE: the Remote IDE for Dyalog APL"
+  homepage "https://github.com/Dyalog/ride"
 
-  app ""
+  app "RIDE"
 end
