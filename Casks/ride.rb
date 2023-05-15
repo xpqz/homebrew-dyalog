5b4d7695b23af7b62552b2a11e0286d5057424c6
Stefan Kruger <stefan@dyalog.com>
2023-05-15 14:31:16 +0100

    testing RIDE cask

---
+++ Casks/ride.rb
@@ -0,0 +1,11 @@
+cask "ride" do
+  version "4.4.3732_mac"
+  sha256 "e3aba4ab80396f6902ddde67369d221bd855071596edda3ab410374bdda7ced6"
+
+  url "https://github.com/Dyalog/ride/releases/download/v4.4.3732/ride-#{version}.pkg"
+  name "ride"
+  desc ""
+  homepage ""
+
+  app ""
+end
